41e84b5191075d5a7f321df5aaf1d0b8b60fe598
parth-gz <gaikwadparth2304@gmail.com>
2024-09-30 12:43:59 +0530

    modified coin change file

--- coin_change.cpp
+++ coin_change.cpp
@@ -2,24 +2,18 @@
 #include <vector>
 using namespace std;
 
-// Returns the count of ways we can
-// sum coinValues[0...numCoins-1] coins to get targetSum
+
 int findWaysToMakeSum(const vector<int>& coinValues, int numCoins, int targetSum) {
-  
-    // If targetSum is 0 then there is 1 solution
-    // (do not include any coin)
-    if (targetSum == 0) return 1;
-
-    // 0 ways in the following two cases
-    if (targetSum < 0 || numCoins == 0) return 0;
-
-    // count is sum of solutions (i)
-    // including coinValues[numCoins-1] (ii) excluding coinValues[numCoins-1]
+    // If targetSum is 0 then there is 1 solution, i.e. no coin selected
+    if (targetSum == 0) 
+        return 1;
+    // if the sum is negative or we don't have any coins then there will be 0 solutions, therefore return 0 
+    if (targetSum < 0 || numCoins == 0) 
+        return 0;
+    // count the number of solutions by calling the function recursively (once including the current coin and once excluding the current coin)
     return findWaysToMakeSum(coinValues, numCoins, targetSum - coinValues[numCoins - 1]) + 
            findWaysToMakeSum(coinValues, numCoins - 1, targetSum);
 }
-
-// Driver code
 int main() {
     vector<int> coinValues = {1, 2, 3};
     int targetSum = 5;
